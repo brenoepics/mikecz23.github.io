0f019e7f73574176632cf621138192c9a6558578
MikeCZ23 <45541448+MikeCZ23@users.noreply.github.com>
2024-03-14 22:05:34 +0100

    Update config.ts

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -35,8 +35,8 @@ export default defineConfig({
       // dropdown
       { text: 'readme', items: [
       { text: 'Voices of the Void', link: '/readme/VotV'},
-      { text: 'Progressbar95', link: '/readme/progressbar95'}]
-       // 
+      { text: 'Progressbar95', link: '/readme/progressbar95'}],
+
       { text: "Novinky", link: "/novinky/", activeMatch: "/novinky/"},
       { text: "Hollow Knight", link: "/hollow/", activeMatch: "/hollow/" },
       { text: "Plague Inc", link: "/plague/", activeMatch: "/plague/" },
