4c7aff983f9457e3596ed486d867f76a22ce4654
MikeCZ23 <45541448+MikeCZ23@users.noreply.github.com>
2024-03-14 11:10:33 +0100

    .

---
+++ .vitepress/config.ts
@@ -0,0 +1,55 @@
+import { defineConfig } from "vitepress"
+import FastGlob from "fast-glob";
+import grayMatter from "gray-matter";
+import { readFile } from "node:fs/promises";
+import { basename } from "node:path";
+
+const posts: any[] = [];
+for (const source of await FastGlob("novinky/*-*.md")) {
+  const content = await readFile(source, "utf-8");
+  const matter = grayMatter(content);
+  posts.push({
+    text: matter.data.title,
+    link: `/novinky/${basename(source, ".md")}.html`
+  });
+}
+
+// https://vitepress.dev/reference/site-config
+export default {
+  srcDir: './src'
+}
+export default defineConfig({
+  title: "Hollow Knight – Čeština",
+  description: "Stránka fanouškovského překladu hry Hollow Knight.",
+  base: "/test/hollow-knight/",
+  head: [
+    ["link", { rel: "icon", href: "/hollow-knight/favicon.ico" }],
+  ],
+  themeConfig: {
+    docFooter: {
+      prev: 'Předchozí stránka',
+      next: 'Následující stránka'
+    },
+
+    darkModeSwitchLabel: "Vzhled",
+
+    nav: [
+      { text: "Úvod", link: "/" },
+      { text: "Novinky", link: "/novinky/", activeMatch: "/novinky/" },
+    ],
+
+    outline: {
+      label: "Obsah"
+    },
+
+    sidebar: {
+      "/novinky/": posts
+    },
+
+    socialLinks: [
+      { icon: "facebook", link: "#" },
+      { icon: "instagram", link: "#" },
+      { icon: "github", link: "#" }
+    ]
+  }
+});
