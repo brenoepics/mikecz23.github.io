56917a26e6e1296f9cd44d13e3f92175e7aed447
MikeCZ23 <45541448+MikeCZ23@users.noreply.github.com>
2024-03-24 11:58:21 +0100

    Update config.ts

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -30,13 +30,13 @@ export default defineConfig({
       next: 'Následující stránka'
     },
 
-    lastUpdated: {
-      text: 'Updated at',
-      formatOptions: {
-        dateStyle: 'short'
+   // lastUpdated: {
+   //   text: 'Updated at',
+   //   formatOptions: {
+   //     dateStyle: 'short'
        // timeStyle: 'none'
-      }
-    },
+  //    }
+  //  },
     
   //  footer: {
   //  message: '<a href="https://opensource.org/licenses/MIT"><img src="https://img.shields.io/badge/License-MIT-yellow.svg"></a>',
