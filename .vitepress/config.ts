81c6d906c2e6879fef1428c2ada6aad019934d82
MikeCZ23 <45541448+MikeCZ23@users.noreply.github.com>
2024-03-24 11:40:15 +0100

    Update config.ts

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -33,7 +33,7 @@ export default defineConfig({
     lastUpdated: {
       text: 'Updated at',
       formatOptions: {
-        dateStyle: 'short' // full
+        dateStyle: 'timeago'
        // timeStyle: 'none'
       }
     },
