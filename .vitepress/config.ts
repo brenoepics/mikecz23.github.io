488b28fde306f938c6882bf43605f47d42fbb8c5
MikeCZ23 <45541448+MikeCZ23@users.noreply.github.com>
2024-03-24 12:47:26 +0100

    Update config.ts

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -17,6 +17,9 @@ for (const source of await FastGlob("novinky/*-*.md")) {
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
  // lastUpdated: true,
+  theme: hopeTheme({
+    darkmode: "enable",
+  }),
   
   title: "MikeCZ - Češtiny",
   description: "Stránka fanouškovského překladu hry.",
