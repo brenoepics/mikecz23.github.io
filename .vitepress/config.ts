04fa000749cdfc6e316cac70c4eb4c2bad6e6624
MikeCZ23 <45541448+MikeCZ23@users.noreply.github.com>
2024-04-07 13:47:04 +0200

    Update config.ts

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -19,7 +19,7 @@ export default defineConfig({
  // lastUpdated: true,
    sitemap: {
     hostname: 'https://mikecz23.github.io/'
-  }
+  },
   
   title: "MikeCZ - Češtiny",
   description: "Stránka fanouškovského překladu hry.",
