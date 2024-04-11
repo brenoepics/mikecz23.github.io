7a1b3141d444d73ee091144221136256b08df90c
MikeCZ23 <45541448+MikeCZ23@users.noreply.github.com>
2024-04-11 21:46:40 +0200

    Update config.ts

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -54,7 +54,7 @@ export default defineConfig({
     },
 
      editLink: {
-      pattern: 'https://github.com/vuejs/vitepress/edit/main/docs/:path',
+      pattern: 'https://github.com/mikecz23/mikecz23.github.io/edit/main/readme/silksong.md/:path',
       text: 'Edit this page on GitHub'
     },
 
