3ab7463875942cd45ae69febd9582d1f3afb9476
MikeCZ23 <45541448+MikeCZ23@users.noreply.github.com>
2024-04-17 20:13:15 +0200

    Update gisqus.vue

--- .vitepress/theme/components/gisqus.vue
+++ .vitepress/theme/components/gisqus.vue
@@ -42,7 +42,7 @@ if (typeof modeToggle !== "undefined") {
       data-emit-metadata="0"
       data-input-position="top"
       data-lang="en"
-      data-theme="https://cdn.jsdelivr.net/gh/MikeCZ23/mikecz23.github.io@latest/.vitepress/theme/giscus.css"
+      data-theme="changeGiscusTheme ()" 
       data-loading="lazy"
       async
     />
