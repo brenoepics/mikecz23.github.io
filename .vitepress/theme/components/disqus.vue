c9cd03f9f8c9a2a7bd6ad0d6ba9862f0a595b37d
MikeCZ23 <45541448+MikeCZ23@users.noreply.github.com>
2024-04-17 12:23:46 +0200

    Update disqus.vue

--- .vitepress/theme/components/disqus.vue
+++ .vitepress/theme/components/disqus.vue
@@ -1,20 +1,11 @@
 <script setup lang="ts">
 import { useData } from 'vitepress'
 
-const { frontmatter, title } = useData()
+const { title } = useData()
 </script>
 
 <template>
-  <!--
-    The `class="giscus"` ensures that the comment section is inserted in the right place,
-    so that `v-if` can unload the comment section precisely.
-  -->
-  <div v-if="frontmatter.comments !== false" :key="title" class="giscus">
-    <!--
-      It doesn't make sense to use `v-if` to unmount `script` here,
-      what needs to be uninstalled is the giscus comment section that has been inserted into the DOM.
-    -->
-    
+  <div :key="title" class="giscus">
     <component
       :is="'script'"
       src="https://giscus.app/client.js"
