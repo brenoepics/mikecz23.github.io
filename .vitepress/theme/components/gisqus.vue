794ca9ca3c81d5b3442042a19c89415fab024ddb
MikeCZ23 <45541448+MikeCZ23@users.noreply.github.com>
2024-04-18 15:04:45 +0200

    Update gisqus.vue

--- .vitepress/theme/components/gisqus.vue
+++ .vitepress/theme/components/gisqus.vue
@@ -1,32 +1,35 @@
-<script setup>
-import DefaultTheme from "vitepress/theme"
+<script setup lang="ts">
+import { useData } from 'vitepress'
+import Giscus from '@giscus/vue'
 
-const { Layout } = DefaultTheme
+const { frontmatter, title } = useData()
 </script>
 
+<!-- https://cdn.jsdelivr.net/gh/MikeCZ23/mikecz23.github.io@main/.vitepress/theme/giscus.css -->
 <template>
-  <Layout>
-    <template #doc-after>
-      <div class="giscus">
-        <Giscus
-          :key="giscus"
-          host="https://giscus.app"
-          repo="XboxYan/xy-ui"
-          repoId="MDEwOlJlcG9zaXRvcnk2MjYzODgxNA=="
-          categoryId="MDE4OkRpc2N1c3Npb25DYXRlZ29yeTMyOTYxODk2" 
-          :theme="isDark ? 'dark' : 'light'"
-          lang="zh-CN"
-          loading="lazy"
-          strict="1"
-          mapping="title"
-          crossorigin="anonymous"
-        />
-      </div>
-    </template>
-  </Layout>
+  <div v-if="frontmatter.comments !== false" :key="title" class="giscus">
+    <component
+      :is="'script'"
+      src="https://giscus.app/client.js"
+      data-repo="MikeCZ23/mikecz23.github.io"
+      data-repo-id="R_kgDOLf7Wgg"
+      data-category="General"
+      data-category-id="DIC_kwDOLf7Wgs4CeLpZ"
+      data-mapping="pathname"
+      data-strict="0"
+      data-reactions-enabled="1"
+      data-emit-metadata="0"
+      data-input-position="top"
+      data-lang="en"
+      :data-theme="isDark ? 'dark' : 'light'" 
+      data-loading="lazy"
+      async
+    />
+  </div>
 </template>
 
-<script>
+
+<script lang="ts">
 import { watch } from 'vue'
 import { useRoute, useData } from 'vitepress'
 
