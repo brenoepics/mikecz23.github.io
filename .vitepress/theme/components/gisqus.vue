51fd189c510ddfbbd5476a251809c58d626687c7
MikeCZ23 <45541448+MikeCZ23@users.noreply.github.com>
2024-04-18 15:59:56 +0200

    Update gisqus.vue

--- .vitepress/theme/components/gisqus.vue
+++ .vitepress/theme/components/gisqus.vue
@@ -4,8 +4,6 @@ import { useData } from 'vitepress'
 const { frontmatter, title } = useData()
 </script>
 
-<!-- I MUST F5 IF I CHANGE THEME -->
-<!-- https://cdn.jsdelivr.net/gh/MikeCZ23/mikecz23.github.io@main/.vitepress/theme/giscus.css -->
 <template>
   <div v-if="frontmatter.comments !== false" :key="title" class="giscus">
     <component
@@ -21,44 +19,9 @@ const { frontmatter, title } = useData()
       data-emit-metadata="0"
       data-input-position="top"
       data-lang="en"
-      :data-theme="isDark ? 'dark' : 'light'" 
+      data-theme="https://cdn.jsdelivr.net/gh/MikeCZ23/mikecz23.github.io@main/.vitepress/theme/giscus.css" 
       data-loading="lazy"
       async
     />
   </div>
 </template>
-
-
-<script lang="ts">
-import { watch } from 'vue'
-import { useRoute, useData } from 'vitepress'
-
-export default {
-  mounted() {
-    const vitePressData = useData()
-    this.isDark = vitePressData.isDark
-
-    const route = useRoute() 
-    watch(route, () => {
-      this.giscus = !this.giscus
-    })
-  },
-  data() {
-    return {
-      giscus: true,
-      isDark: false
-    }
-  }
-}
-</script>
-
-<style>
-img.pv {
-  margin-top: 1em;
-}
-
-div.giscus {
-  margin-top: 2em;
-}
-
-</style>
