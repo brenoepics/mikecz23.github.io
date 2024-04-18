b7fb8229b6878703bd78027e838ad44efed7be02
MikeCZ23 <45541448+MikeCZ23@users.noreply.github.com>
2024-04-18 14:54:24 +0200

    Update gisqus.vue

--- .vitepress/theme/components/gisqus.vue
+++ .vitepress/theme/components/gisqus.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { useData } from 'vitepress'
+import Giscus from '@giscus/vue'
 
 const { frontmatter, title } = useData()
 </script>
@@ -26,3 +27,38 @@ const { frontmatter, title } = useData()
     />
   </div>
 </template>
+
+
+<script lang="ts">
+import { watch } from 'vue'
+import { useRoute, useData } from 'vitepress'
+
+export default {
+  mounted() {
+    const vitePressData = useData()
+    this.isDark = vitePressData.isDark
+
+    const route = useRoute() 
+    watch(route, () => {
+      this.giscus = !this.giscus
+    })
+  },
+  data() {
+    return {
+      giscus: true,
+      isDark: false
+    }
+  }
+}
+</script>
+
+<style>
+img.pv {
+  margin-top: 1em;
+}
+
+div.giscus {
+  margin-top: 2em;
+}
+
+</style>
