fdc0f9b98cb515e14dbccd430ce47fd6d631a74e
MikeCZ23 <45541448+MikeCZ23@users.noreply.github.com>
2024-04-20 21:26:33 +0200

    Update config.ts

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -88,10 +88,10 @@ export default defineConfig({
   //    }
   //  },
     
-  //  footer: {
-  //  message: '<a href="https://opensource.org/licenses/MIT"><img src="https://img.shields.io/badge/License-MIT-yellow.svg"></a>',
-  //  copyright: 'Copyright © 2024-present'
-  //  },
+    footer: {
+    message: '<a href="https://opensource.org/licenses/MIT"><img src="https://img.shields.io/badge/License-MIT-yellow.svg"></a> <br> <a href="https://www.toplist.cz/stat/1836145/"><img src="https://toplist.cz/count.asp?id=1836145&logo=btn&njs=1" border="0" alt="TOPlist" width="80" height="15" /></a>',
+    copyright: 'Copyright © 2024-present'
+    },
     
     darkModeSwitchLabel: "Vzhled",
     lightModeSwitchTitle: "Přepnout do světlého režimu",
