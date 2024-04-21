9ac1a0ef480154e7e5bff474f4e5688e826687a3
MikeCZ23 <45541448+MikeCZ23@users.noreply.github.com>
2024-04-21 21:36:06 +0200

    Update index.ts

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -11,8 +11,7 @@ import PDateString from "./components/PDateString.vue";
 import PTeamMembers from "./components/PTeamMembers.vue";
 import Layout from "./components/Layout.vue";
 import vitepressBackToTop from 'vitepress-plugin-back-to-top'
-import './BtP-style.css';
-import '../../readme/components/theme.css'
+import './BtP-style.css'
 
 export default {
   Layout,
