acc15ee3712cfe6f615b7de98bee5102512c9cb6
MikeCZ23 <45541448+MikeCZ23@users.noreply.github.com>
2024-09-13 18:52:13 +0200

    Update index.ts

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -13,25 +13,11 @@ import PTeamMembers from "./components/PTeamMembers.vue";
 import Layout from "./components/Layout.vue";
 import vitepressBackToTop from '../../plugins/vitepress-plugin-back-to-top/';
 import './BtP-style.css'
-import vitepressMusic from 'vitepress-plugin-music'
-import '../../plugins/vitepress-plugin-music/dist/index.css'
 
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import 'element-plus/theme-chalk/dark/css-vars.css'
 
-const playlist = [
-  {
-    name: 'Hollow Knight',
-    author: 'TyKim',
-    file: 'https://od.lk/s/MTBfMjQ5NTEzODE4Xw/hollowknight.mp3',
-  },
-  {
-    name: 'Hollow Knight (2)',
-    author: 'Man on the Internet',
-    file: 'https://od.lk/s/MTBfMjQ5NTE1MzQ2Xw/hollowknight2.mp3',
-  }
-]
 
 export default {
   Layout: () => h(Layout),
