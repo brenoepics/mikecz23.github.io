1a612339c9a5f666db9947531ae61799e26bd8b8
Breno A. <git@breno.tech>
2024-10-15 10:47:47 -0300

    Update config.ts

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -57,7 +57,7 @@ export default defineConfig({
     }),
     RssPlugin(RSS),
   ],
-  ssr: { noExternal: ['element-plus', /\.css$/, /\.vue$/, /^vitepress-carbon/, /^vitepress/]
+  ssr: { noExternal: ['element-plus', /\.css$/, /\.vue$/, /^vitepress-carbon/, /^vitepress/, '@docsearch/css']
     },
   css: {
     postcss: {
