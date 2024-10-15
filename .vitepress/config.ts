7f3580e03f6a0d2ce3533635b630fed0ac88bb8b
MikeCZ23 <45541448+MikeCZ23@users.noreply.github.com>
2024-10-15 15:26:56 +0200

    Update config.ts

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -57,12 +57,7 @@ export default defineConfig({
     }),
     RssPlugin(RSS),
   ],
-  ssr: { noExternal: [
-      'element-plus',               
-      /\.css$/,                     
-      /\.vue$/,                     
-      /^vitepress-carbon/
-  		]
+  ssr: { noExternal: ['element-plus', /\.css$/, /\.vue$/, /^vitepress-carbon/]
     },
   css: {
     postcss: {
